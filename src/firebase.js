0486967cb699df81135bc9ae47bc220c47634625
Luichix <luigui.rex@gmail.com>
2021-01-14 09:20:49 -0600

    create-react-app and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDjndKFyQquRDBWlXfDw04Xaie1u5DBHmY",
+    authDomain: "challenge-1c21a.firebaseapp.com",
+    databaseURL: "https://challenge-1c21a-default-rtdb.firebaseio.com",
+    projectId: "challenge-1c21a",
+    storageBucket: "challenge-1c21a.appspot.com",
+    messagingSenderId: "356843030277",
+    appId: "1:356843030277:web:b650852c71d9293091d96c",
+    measurementId: "G-EDSE87GHTL"
+  };
